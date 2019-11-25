271b18cd6e3e449e1a2c05409eb725180be853fe
Oskarinio <zielasoon@gmail.com>
2019-11-25 12:38:39 +0100

    small fixes to links

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#welcome'
-  get '/pages/portfolio' => 'pages#portfolio'
+  get '/pages/portfolio' => 'pages#portfolio', as: :portfolio
   get '/pages/about' => 'pages#about'
   get '/pages/contact' => 'pages#contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
